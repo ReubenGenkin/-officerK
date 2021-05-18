f8b5615c0c5ee7f1e576b85eb067cf6dfb174cf7
Reuben <rleibgenkin@gmail.com>
2021-05-18 18:29:16 -0400

    implemented changes to input field color based on time

--- assets/js.js
+++ assets/js.js
@@ -1,7 +1,61 @@
-
-
-
-
 var now = JSON.stringify(moment().format("dddd MMMM, YYYY"));
 $("#currentDay").append(now);
 
+var time = parseInt(moment().format("HH"));
+console.log(time);
+
+$(".time-block").each(function () {
+    var timeDiv = parseInt(
+        $(this)
+            .attr("id")
+            .split("-")[1]
+    );
+
+    console.log(timeDiv)
+    if (timeDiv < time) {
+        //add class to past div use "this"
+        $(this).find('input').addClass('future');
+
+    } else if (timeDiv == time) {
+        //add class to present div
+        $(this).find('input').addClass('present');
+
+    } else {
+        //add class to future div
+        $(this).find('input').addClass('past');
+    }
+})
+
+$('.saveBtn').each().click(function () {
+    // $(".textarea").each(function() {
+    input123 = JSON.stringify($(this));
+    saveINp = localStorage.setItem("key1", input123);
+    console.log(input123);
+});
+// });
+
+
+
+
+
+
+// function store() {
+   
+//     $(".time-block").cu(function () {
+
+//     var timeBlockId = parseInt($(this).attr('id').split("-")[1])
+//     console.log(timeBlockId);
+
+//     var buttId = parseInt($(this).find('button').attr("id").split("-")[1]);
+//     console.log(buttId);
+
+//     if (buttId == timeBlockId) {
+//         inputVal = $(this).find('input').val()
+//     localStorage.setItem("server", inputVal);
+    
+//     }
+// })
+// }
+
+// var generateBtn = document.querySelector(".saveBtn");
+// generateBtn.addEventListener("click", store);
