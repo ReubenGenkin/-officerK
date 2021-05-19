3df4a812d64f283df50cd736acd8e6034c0558b1
Reuben <rleibgenkin@gmail.com>
2021-05-18 23:19:39 -0400

    added local storage and made text content remain on refresh

--- assets/js.js
+++ assets/js.js
@@ -2,7 +2,6 @@ var now = JSON.stringify(moment().format("dddd MMMM, YYYY"));
 $("#currentDay").append(now);
 
 var time = parseInt(moment().format("HH"));
-console.log(time);
 
 $(".time-block").each(function () {
     var timeDiv = parseInt(
@@ -11,51 +10,31 @@ $(".time-block").each(function () {
             .split("-")[1]
     );
 
-    console.log(timeDiv)
     if (timeDiv < time) {
-        //add class to past div use "this"
         $(this).find('input').addClass('future');
 
     } else if (timeDiv == time) {
-        //add class to present div
         $(this).find('input').addClass('present');
 
     } else {
-        //add class to future div
         $(this).find('input').addClass('past');
+    }
+
+    var timeId = $(this)
+        .attr("id")
+
+    var timeContent = localStorage.getItem(timeId);
+
+    var timeText = $(this).find('input').val()
+
+    if (timeText !== null) {
+        $(this).find('input').val(timeContent);
     }
 })
 
-$('.saveBtn').each().click(function () {
-    // $(".textarea").each(function() {
-    input123 = JSON.stringify($(this));
-    saveINp = localStorage.setItem("key1", input123);
-    console.log(input123);
+$('.saveBtn').click(function () {
+    var timeKey = $(this).parent().attr("id");
+    var doItem = $(this).siblings(".textarea").val();
+
+    localStorage.setItem(timeKey, doItem);
 });
-// });
-
-
-
-
-
-
-// function store() {
-   
-//     $(".time-block").cu(function () {
-
-//     var timeBlockId = parseInt($(this).attr('id').split("-")[1])
-//     console.log(timeBlockId);
-
-//     var buttId = parseInt($(this).find('button').attr("id").split("-")[1]);
-//     console.log(buttId);
-
-//     if (buttId == timeBlockId) {
-//         inputVal = $(this).find('input').val()
-//     localStorage.setItem("server", inputVal);
-    
-//     }
-// })
-// }
-
-// var generateBtn = document.querySelector(".saveBtn");
-// generateBtn.addEventListener("click", store);
